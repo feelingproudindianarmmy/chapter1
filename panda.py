42128724f2942995cadbdcb2284962e3e85feb5e
feelingproudindianarmy <celestialh4@gmail.com>
2024-12-05 17:00:25 +0545

    manipulation

---
+++ panda.py
@@ -0,0 +1,8 @@
+import pandas
+mydataset={
+    'cars':["BMW","VOLVO","FORD"],
+    'passing':[1,2,3]
+}
+my=pandas.DataFrame(mydataset)
+print(my)
+
---
+++ random.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+random_ = np.random.random(100)  
+print(random)
